4aaa86a601bbc56f72e265c0dbc2850760c6afd3
weifei365 <weifei365@gmail.com>
2018-10-17 16:22:39 +0800

    移除国际化的支持

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,16 +10,10 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
+    return <h1>Home Page</h1>;
   }
 }
--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -10,17 +10,10 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class NotFound extends React.PureComponent {
   render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
+    return <h1>Not Found</h1>;
   }
 }
